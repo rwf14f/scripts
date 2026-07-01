e0513c7997f43000f167cd10fe90890a24591359
Robert Frank <robert.frank@manchester.ac.uk>
2026-07-01 09:24:40 +0100

    Change the collection of PG state values.

--- ceph/python/ceph-pg-exporter.py
+++ ceph/python/ceph-pg-exporter.py
@@ -8,7 +8,11 @@ from ceph.utils import ceph_stamp_to_datetime
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig()
-LOG.level = logging.WARNING
+#LOG.level = logging.WARNING
+LOG.level = logging.DEBUG
+
+def log_timediff(msg, s):
+    LOG.debug(f'{msg}: {(time.time_ns() - s)/1000000}')
 
 class CephPGStatCollector(object):
 
@@ -50,6 +54,10 @@ class CephPGStatCollector(object):
         'ceph_pg_last_deep_scrub_stamp': {'desc': 'Timestamp of last deep scrub', 'key': 'last_deep_scrub_stamp'},
         'ceph_pg_last_clean_scrub_stamp': {'desc': 'Timestamp of last clean scrub', 'key': 'last_clean_scrub_stamp'},
     }
+    _pg_state_gauges = {}
+    for state in _pg_states:
+        desc = f'PG has state {state}'
+        _pg_state_gauges[f'ceph_pg_state_{state}'] = { 'desc': desc, 'state': state}
     _pg_counters = {
         'ceph_pg_reported_seq': {'desc': 'Reported sequence number', 'key': 'reported_seq'},
         'ceph_pg_reported_epoch': {'desc': 'Reported epoch number', 'key': 'reported_epoch'},
@@ -63,9 +71,11 @@ class CephPGStatCollector(object):
         _pg_sum_counters[f'ceph_pg_stat_sum_{key}'] = { 'desc': desc, 'key': key }
 
     def collect(self):
+        s = st = time.time_ns()
         pgs = PGs(rados=self._rados)
+        log_timediff('PGs object', s)
         pgInfo = InfoMetricFamily('ceph_pg', 'General PG info', labels = ['pgid'])
-        pgInfoState = InfoMetricFamily('ceph_pg_state', 'PG state information', labels = ['pgid'])
+        s = time.time_ns()
         for pgstat in pgs.pg_stats:
             pgid = pgstat['pgid']
             pool_id = int(pgid.split('.')[0])
@@ -75,34 +85,47 @@ class CephPGStatCollector(object):
                 'pool_name': pool_name,
                 'version': pgstat['version'],
             })
-            pg_states = pgstat['state'].split('+')
-            states = {}
-            for state in self._pg_states:
-                states[state] = str(int(state in pg_states))
-            pgInfoState.add_metric([pgid], states)
         yield pgInfo
-        yield pgInfoState
+        log_timediff('pgInfo', s)
+        s = time.time_ns()
+        for metric in self._pg_state_gauges:
+            pgGauge = GaugeMetricFamily(metric, self._pg_state_gauges[metric]['desc'], labels=['pgid'])
+            for pgstat in pgs.pg_stats:
+                pg_states = pgstat['state'].split('+')
+                have_state = 1 if self._pg_state_gauges[metric]['state'] in pg_states else 0
+                pgGauge.add_metric([pgstat['pgid']], have_state)
+            yield pgGauge
+        log_timediff('State gauges', s)
+        s = time.time_ns()
         for metric in self._pg_gauges:
             pgGauge = GaugeMetricFamily(metric, self._pg_gauges[metric]['desc'], labels=['pgid'], unit=self._pg_gauges[metric].get('unit', ''))
             for pgstat in pgs.pg_stats:
                 pgGauge.add_metric([pgstat['pgid']], self._pg_gauges[metric].get('convert', lambda x: x)(pgstat[self._pg_gauges[metric]['key']]))
             yield pgGauge
+        log_timediff('Gauges', s)
+        s = time.time_ns()
         for metric in self._pg_stamp_gauges:
             pgGauge = GaugeMetricFamily(metric, self._pg_stamp_gauges[metric]['desc'], labels=['pgid'], unit='utctimestamp')
             for pgstat in pgs.pg_stats:
                 pgGauge.add_metric([pgstat['pgid']], ceph_stamp_to_datetime(pgstat[self._pg_stamp_gauges[metric]['key']]).timestamp())
             yield pgGauge
+        log_timediff('Stamp gauges', s)
+        s = time.time_ns()
         for metric in self._pg_counters:
             pgCounter = CounterMetricFamily(metric, self._pg_counters[metric]['desc'], labels=['pgid'])
             for pgstat in pgs.pg_stats:
                 pgCounter.add_metric([pgstat['pgid']], pgstat[self._pg_counters[metric]['key']])
             yield pgCounter
+        log_timediff('Counters', s)
+        s = time.time_ns()
         for metric in self._pg_sum_counters:
             
             pgCounter = CounterMetricFamily(metric, self._pg_sum_counters[metric]['desc'], labels=['pgid'])
             for pgstat in pgs.pg_stats:
                 pgCounter.add_metric([pgstat['pgid']], pgstat['stat_sum'][self._pg_sum_counters[metric]['key']])
             yield pgCounter
+        log_timediff('Sum counters', s)
+        log_timediff('Collect total', st)
 
 if __name__ == '__main__':
     cluster = PGs.rados_connect()
